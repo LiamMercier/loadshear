ede1d958159bfcb528a20bc5fb8231c78a1b24c3
Liam Mercier <LiamMercier@proton.me>
2025-12-17 09:23:42 -0800

    Moved good DSL data generation to test fixtures file

---
+++ tests/cli-tests.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+#include "test-helpers.h"
+#include "test-fixtures.h"
+
+TEST(CLITests, ValidPlanGeneration)
+{
+    // TODO: grab known good data.
+    DSLData correct_data = get_simple_valid_script_data();
+}
---
+++ tests/test-fixtures.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "script-structs.h"
+
+DSLData get_simple_valid_script_data();
+
